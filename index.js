13969350ca884ee42651e60f2cd5bfe0708e4be7
Chantal Lengua <43709607+Chantallengua@users.noreply.github.com>
2020-10-27 02:39:39 +0100

    up

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var pass1 = prompt('Indovina la password:','');
 while (testV < 300) {
 if (!pass1)
 history.go(-1);
-if (pass1.toLowerCase() == "aquila") {
+if (pass1.toLowerCase() == "rocher") {
 alert('Esatt.');
 window.open('index2.html');
 break;
 }
 testV+=1;
 var pass1 =
-prompt('Riprova','');
+prompt('Riprova, o vat accir:','');
 }
 if (pass1.toLowerCase()!="password" & testV ==300)
 history.go(-1);
